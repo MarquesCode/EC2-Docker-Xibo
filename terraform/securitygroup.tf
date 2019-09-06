dbb5d028069c8f6c6bc5d08a019d57852cd02273
Marques <marqueslima.dev@gmail.com>
2019-09-06 09:05:18 -0300

    new syntax

--- terraform/securitygroup.tf
+++ terraform/securitygroup.tf
@@ -1,15 +1,16 @@
 resource "aws_security_group" "allow-ssh" {
-  name = "allow-ssh"
-  vpc_id = "${aws_vpc.main.id}"
+  name   = "allow-ssh"
+  vpc_id = aws_vpc.main.id
+
   #vpc_id      = "vpc-c84f9cb0"
   description = "security group that allows ssh and all egress traffic"
   ingress {
-      from_port = 22
-      to_port = 22
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-  } 
-   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
@@ -21,7 +22,7 @@ resource "aws_security_group" "allow-ssh" {
     protocol    = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-    ingress {
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -33,18 +34,19 @@ resource "aws_security_group" "allow-ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-    egress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-  
-tags {
-        Name = "DEV Teste Infraesturura Agil"
-        Projeto = "Infraestrutura como codigo"
-        Curso = "Redes de Computadores"
-        Materia = "Projeto Integrador 3"
-        Gerente_do_Projeto = "Professor Pablo Menezes"
-    }
+
+  tags = {
+    Name               = "DEV Teste Infraesturura Agil"
+    Projeto            = "Infraestrutura como codigo"
+    Curso              = "Redes de Computadores"
+    Materia            = "Projeto Integrador 3"
+    Gerente_do_Projeto = "Professor Pablo Menezes"
+  }
 }
+
